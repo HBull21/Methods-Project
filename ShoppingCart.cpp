c6e259d9fd8a3ebf0c8971f207b29a43e168fc4c
HBull21 <116595153+HBull21@users.noreply.github.com>
2022-11-08 13:05:46 -0600

    shoppingcart.cpp

---
+++ ShoppingCart.cpp
@@ -0,0 +1,33 @@
+#include "ShoppingCart.h"
+
+void ShoppingCart::getUserID() {
+
+}
+
+int ShoppingCart::getQuantity() {
+
+}
+
+int ShoppingCart::changeQuantity() {
+
+}
+
+int ShoppingCart::addItem(ISBN) {
+
+}
+
+int ShoppingCart::removeItem(ISBN) {
+
+}
+
+double ShoppingCart::totalCost() {
+
+}
+
+void ShoppingCart::viewCart() {
+
+}
+
+void ShoppingCart::checkout () {
+
+}
